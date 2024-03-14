8ed086719d7051bd507b0d8939dba56ee52f583b
LeedsByExample <leeda.minkin@yahoo.com>
2024-03-13 19:55:48 -0500

    Created project

---
+++ src/main/java/com/leeda/ButtonClicker/ButtonClickerApplication.java
@@ -0,0 +1,13 @@
+package com.leeda.ButtonClicker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ButtonClickerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ButtonClickerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leeda/ButtonClicker/ButtonClickerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leeda.ButtonClicker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ButtonClickerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
